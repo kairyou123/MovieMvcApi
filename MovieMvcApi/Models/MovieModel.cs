986ce1ee3a9b119eee1fed789da82a95764262f3
unknown <kairyou522@gmail.com>
2020-03-31 07:14:36 +0700

    init

---
+++ MovieMvcApi/Models/MovieModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MovieMvcApi.Models
+{
+    public class MovieModel
+    {
+        public String Title { get; set; }
+
+        public String Genre { get; set; }
+    }
+}
